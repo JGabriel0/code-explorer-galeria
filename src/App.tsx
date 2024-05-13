7dada04ce899559fda11111714036ab2146c3f92
JGabriel0 <jgabriel5076@gmail.com>
2024-05-13 19:19:15 -0300

    hover na tela media e active na tela pequena

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,14 @@ export function App() {
     <section className="bg-neutral-300 w-full h-screen flex justify-center items-center overflow-x-auto">
       <div className="flex items-center gap-6">
         {images.map((image, index) => (
-          <div className="w-[110px] h-[350px] rounded-md cursor-pointer ease-linear duration-75 grayscale-[.75] active:grayscale-0 active:w-[400px] active:h-[400px]">
+          <div
+            className="w-[110px] h-[250px] md:h-[350px] rounded-md cursor-pointer ease-linear duration-75 grayscale-[.75] active:grayscale-0 hover:grayscale-0 active:w-[250px] active:h-[250px] md:hover:w-[400px] md:hover:h-[400px]"
+            key={index}
+          >
             <img
               className="w-full h-full object-cover rounded-md"
               src={image}
               alt={String(index)}
-              key={index}
             />
           </div>
         ))}
