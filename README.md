227189e50eef2acd14931670aca1c1b9c6dfe6be
JGabriel0 <jgabriel5076@gmail.com>
2024-05-13 19:08:51 -0300

    commit initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto de Galeria do Code Explorer
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import Imagem1 from "./assets/imagem-1.jpg";
+import Imagem2 from "./assets/imagem-2.jpg";
+import Imagem3 from "./assets/imagem-3.jpg";
+import Imagem4 from "./assets/imagem-4.jpg";
+import Imagem5 from "./assets/imagem-5.jpg";
+import Imagem6 from "./assets/imagem-6.jpg";
+import Imagem7 from "./assets/imagem-7.jpg";
+
+const images = [Imagem1, Imagem2, Imagem3, Imagem4, Imagem5, Imagem6, Imagem7];
+
+export function App() {
+  return (
+    <section className="bg-neutral-300 w-full h-screen flex justify-center items-center overflow-x-auto">
+      <div className="flex items-center gap-6">
+        {images.map((image, index) => (
+          <div className="w-[110px] h-[350px] rounded-md cursor-pointer ease-linear duration-75 grayscale-[.75] active:grayscale-0 active:w-[400px] active:h-[400px]">
+            <img
+              className="w-full h-full object-cover rounded-md"
+              src={image}
+              alt={String(index)}
+              key={index}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
